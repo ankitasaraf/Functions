b5d998015b11ae11543b313691c932c52089dd98
Ankita Saraf <saraf.ankita90@gmail.com>
2018-05-17 21:45:50 +0530

    Create dependencies.r

---
+++ dependencies.r
@@ -0,0 +1,11 @@
+if (!require(tm)) {install.packages("tm")}
+if (!require(wordcloud)) {install.packages("wordcloud")}
+if (!require(igraph)) {install.packages("igraph")}
+if (!require(ggraph)) {install.packages("ggraph")}
+
+library(tm) 
+library(tidyverse)
+library(tidytext)
+library(wordcloud)
+library(igraph)
+library(ggraph)
